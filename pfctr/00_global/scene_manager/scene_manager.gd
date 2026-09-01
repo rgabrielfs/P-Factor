463b30e751781356a1cd855ce7fc38f6e633c214
rgabrielfs <rgabrielfialho@gmail.com>
2026-09-01 16:50:08 -0300

    Added level transition mechanic

---
+++ pfctr/00_global/scene_manager/scene_manager.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+signal load_scene_started
+signal new_scene_ready( target_name: String, offset: Vector2 )
+signal load_scene_finished
+
+func _ready() -> void:
+	await get_tree().process_frame
+	load_scene_finished.emit()
+	pass
+
+func transition_scene( new_scene: String, target_area: String, player_offset: Vector2, dir: String ) -> void:
+	
+	load_scene_started.emit()
+	
+	get_tree().change_scene_to_file( new_scene )
+	
+	await get_tree().scene_changed
+	
+	new_scene_ready.emit( target_area, player_offset )
+	
+	load_scene_finished.emit()
+	
+	pass
--- pfctr/general/level_transition/level_transition.gd
+++ pfctr/general/level_transition/level_transition.gd
@@ -1,7 +1,7 @@
 @tool
 class_name LevelTransition extends Node2D
 
-enum SIDE{ LEFT, RIGHT, UP, DOWN }
+enum SIDE{ LEFT, RIGHT, TOP, DOWN }
 
 @export_range( 2, 12, 1, "or_greater" ) var size : int = 2 :
 	set( value ): 
@@ -18,10 +18,62 @@ enum SIDE{ LEFT, RIGHT, UP, DOWN }
 
 @onready var area_2d: Area2D = $Area2D
 
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	SceneManager.new_scene_ready.connect( _on_new_scene_ready )
+	SceneManager.load_scene_finished.connect( _on_load_scene_finished )
+	pass
+
+func _on_player_entered( _n : Node2D ) -> void:
+	SceneManager.transition_scene( target_level, target_area_name, get_offset( _n ), "left" )
+	pass
+
+func _on_new_scene_ready( target_name: String, offset: Vector2 ) -> void:
+	if target_name == name:
+		var player : Node = get_tree().get_first_node_in_group( "Player" )
+		player.global_position = global_position + offset
+	pass
+
+func _on_load_scene_finished() -> void:
+	area_2d.body_entered.connect( _on_player_entered )
+	pass
 
 func apply_area_settings() -> void:
 	area_2d = get_node_or_null( "Area2D" )
 	if not area_2d:
 		return
-	area_2d.scale.y = size
-	pass 
+	if location == SIDE.LEFT or location == SIDE.RIGHT:
+		area_2d.scale.y = size
+		if location == SIDE.LEFT:
+			area_2d.scale.x = -1
+		else: 
+			area_2d.scale.x = 1
+	else:
+		area_2d.scale.x = size
+		if location == SIDE.TOP:
+			area_2d.scale.y = 1
+		else: 
+			area_2d.scale.y = -1
+	pass
+	
+func get_offset( player: Node2D ) -> Vector2:
+	var offset : Vector2 = Vector2.ZERO
+	var player_pos : Vector2 = player.global_position
+	
+	if location == SIDE.LEFT or location == SIDE.RIGHT:
+	
+		offset.y = player_pos.y - self.global_position.y
+		
+		if location == SIDE.LEFT:
+			offset.x = -4
+		else:
+			offset.x = 4
+	else:
+		offset.y = player_pos.x - self.global_position.x
+		
+		if location == SIDE.TOP:
+			offset.y = -2
+		else:
+			offset.y = 36
+	return offset
